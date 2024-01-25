e545f1615c9bde18adecf4542515e2cae0e697de
jojoner16 <laybats.jonathan@orange.fr>
2024-01-25 12:50:42 +0100

    utilisation de Grommet pour la selection Department

--- src/components/DepartmentDropdown/DepartmentDropdown.js
+++ src/components/DepartmentDropdown/DepartmentDropdown.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Select } from 'grommet';
 import departments from '../../data/departments.json';
 
 const DepartmentsDropdown = ({ onChange, name }) => {
+  const [selectedDepartment, setSelectedDepartment] = useState(
+    departments[0].name
+  );
+
+  const handleSelectChange = (event) => {
+    const selectedValue = event.option.value;
+    setSelectedDepartment(selectedValue);
+    onChange({ target: { name, value: selectedValue } });
+  };
   return (
     <div>
       <label>Department</label>
-      <select title="Select a department" onChange={onChange} name={name}>
-        {departments.map((department) => (
-          <option key={department.name} value={department.name}>
-            {department.name}
-          </option>
-        ))}
-      </select>
+      <Select
+        value={selectedDepartment}
+        onChange={handleSelectChange}
+        options={departments.map((department) => ({
+          label: department.name,
+          value: department.name,
+        }))}
+      />
     </div>
   );
 };
