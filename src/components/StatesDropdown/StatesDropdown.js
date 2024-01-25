ebbeef4a8cda20656a2ac0170a759ca1592856cc
jojoner16 <laybats.jonathan@orange.fr>
2024-01-25 17:56:13 +0100

    selection de l'état

--- src/components/StatesDropdown/StatesDropdown.js
+++ src/components/StatesDropdown/StatesDropdown.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Select } from 'grommet';
 import states from '../../data/states.json';
 
 const StatesDropdown = ({ onChange, name }) => {
-  // Logique d'initialisation du menu déroulant des États
+  const [selectedState, setSelectedState] = useState(states[0].name);
+
+  const handleSelectChange = (event) => {
+    const selectedValue = event.value;
+    setSelectedState(selectedValue);
+    onChange({ target: { name, value: selectedValue.value } });
+  };
+
   return (
     <div>
       <label>State</label>
-      <select title="Select a state" onChange={onChange} name={name}>
-        {states.map((state) => (
-          <option key={state.abbreviation} value={state.abbreviation}>
-            {state.name}
-          </option>
-        ))}
-      </select>
+      <Select
+        value={selectedState}
+        onChange={handleSelectChange}
+        options={states.map((state) => ({
+          label: state.name,
+          value: state.abbreviation,
+        }))}
+      />
     </div>
   );
 };
