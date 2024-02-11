fbc030891d8b7838513dc8b374f94f6699eaf6fd
jojoner16 <laybats.jonathan@orange.fr>
2024-02-11 19:34:28 +0100

    correction de la gestion des pages du tableau et mise en forme

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import { Box, Button, Text } from 'grommet';
+import { Box, Button } from 'grommet';
+import '../../styles/components/pagination.css';
 
-const CustomPagination = ({ currentPage, totalPages, onPrevious, onNext }) => {
+const CustomPagination = ({
+  currentPage,
+  totalPages,
+  onPrevious,
+  onNext,
+  onPageClick,
+}) => {
+  const pageButtons = [];
+
+  // Affichage des boutons de page pour les pages précédentes et la page actuelle
+  for (let i = 1; i <= totalPages; i++) {
+    pageButtons.push(
+      <Button
+        key={i}
+        className={`pageButton ${i === currentPage ? 'currentPage' : ''}`}
+        label={i.toString()}
+        onClick={() => onPageClick(i)}
+        primary={i === currentPage}
+      />
+    );
+  }
+
   return (
     <Box direction="row" gap="small" align="center">
-      <Button disabled={currentPage === 1} onClick={onPrevious}>
+      <Button
+        disabled={currentPage === 1}
+        onClick={onPrevious}
+        className={currentPage === 1 ? '' : 'activeButton'}
+      >
         Previous
       </Button>
-      <Text>{` ${currentPage} `}</Text>
-      <Button disabled={currentPage >= totalPages} onClick={onNext}>
+      {pageButtons}
+      <Button
+        disabled={currentPage >= totalPages}
+        onClick={onNext}
+        className={currentPage >= totalPages ? '' : 'activeButton'}
+      >
         Next
       </Button>
     </Box>
   );
 };
+
 export default CustomPagination;
